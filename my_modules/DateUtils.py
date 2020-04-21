fd3583235f9c80f3979f1c8361e6387db7e972f4
David <davidazar3@gmail.com>
2020-04-21 15:13:41 -0400

    switched time capsule to be 1 day long

--- my_modules/DateUtils.py
+++ my_modules/DateUtils.py
@@ -1,57 +1,40 @@
-import time, datetime
+import time
+import datetime
 import pendulum
 
 
 class DateUtils:
 
-  def __init__(self):
-    print('date util constructor constructor')
+    def __init__(self):
+        print('date util constructor constructor')
 
+    # Creates a pendulum date object with the last day of the current week
+    @staticmethod
+    def get_high_range_for_week():
+        print('high range')
 
-  # Creates a pendulum date object with the last day of the current week
-  @staticmethod
-  def get_high_range_for_week():
-    print('high range')
+        now = pendulum.now()
+        end_of_week = now.end_of('day')
+        print(end_of_week)
 
+        return end_of_week
 
-    now = pendulum.now()
-    end_of_week = now.end_of('week')
-    print(end_of_week)
+    # Creates a pendulum date object with the first day of the current week
+    @staticmethod
+    def get_low_range_for_week():
+        print('low range')
 
-    return end_of_week
+        now = pendulum.now()
+        start_of_week = now.start_of('day')
+        print(start_of_week)
 
-  # Creates a pendulum date object with the first day of the current week
-  @staticmethod
-  def get_low_range_for_week():
-    print('low range')
+        return start_of_week
 
-  
-    now = pendulum.now()
-    start_of_week = now.start_of('week')
-    print(start_of_week)
+    @staticmethod
+    def sort_criteria(message):
+        return pendulum.parse(message.val()['time'])
 
-    return start_of_week
-
-
-  @staticmethod
-  def sort_criteria(message):
-    return pendulum.parse(message.val()['time'])
-    
-
-  @staticmethod
-  def create_date_from_timestamp(timestamp):
-    return pendulum.parse(timestamp)
-
-    
-
-
-
-
-  
-
-  
-
-
-
-
+    @staticmethod
+    def create_date_from_timestamp(timestamp):
+        return pendulum.parse(timestamp)
 
